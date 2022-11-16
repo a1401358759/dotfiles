4d2b9aae81350e0df9c8be9dadfed84104b2e572
yxuefeng <yxuefeng@infervision.com>
2022-11-16 20:33:24 +0800

    polybar 支持多显示器显示

--- .config/polybar/polybar.sh
+++ .config/polybar/polybar.sh
@@ -7,6 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar mybar &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
 
 echo "Polybar runing..."
