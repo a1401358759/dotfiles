84ae2569931ff8b892b718364fc6489e319c848e
yxuefeng <yxuefeng@infervision.com>
2024-09-26 18:15:45 +0800

    add yazi config

---
+++ .config/yazi/init.lua
@@ -0,0 +1,29 @@
+---@diagnostic disable: undefined-global
+-- Starship prompt plugin for yazi
+-- https://github.com/Rolv-Apneseth/starship.yazi
+require("starship"):setup()
+
+-- This plugin provides cross-instance yank ability, which means you can yank
+-- files in one instance, and then paste them in another instance.
+require("session"):setup({
+	sync_yanked = true,
+})
+
+require("yaziline"):setup()
+
+THEME.git_added_sign = ""
+THEME.git_modified_sign = ""
+THEME.git_ignored_sign = ""
+THEME.git_deleted_sign = ""
+THEME.git_untracked_sign = ""
+-- TODO what is this?
+-- THEME.git_updated_sign = ""
+
+-- https://github.com/yazi-rs/plugins/tree/main/git.yazi
+require("git"):setup({})
+
+-- https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+}
